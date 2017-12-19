b587bd1525e8d37462f6bca91d3c410d4b121180
lachesis <mlchiang@gmail.com>
2017-12-19 19:09:29 +0800

    add notice for seedDB option.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 module.exports = {
   sql: {
     database: 'oauth_demo',
-    username: 'user',
+    username: 'root',
     password: 'password',
     dialect: 'mysql', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
     logging: true,
@@ -14,8 +14,8 @@ module.exports = {
   mongo: {
     uri: 'mongodb://gloryque:intranet@192.168.0.200:27017/gloryque_quarc'
   },
-  seedDB:true,
-  seedMongoDB:false,
-  seedDBForce:false,
-  db:'sql' // mongo,sql if you want to use any SQL change dialect above in sql config
+  seedDB: true,  //important: set true only for first time when DB tables not exist!   
+  seedMongoDB: false,
+  seedDBForce: false,
+  db: 'sql' // mongo,sql if you want to use any SQL change dialect above in sql config
 }
